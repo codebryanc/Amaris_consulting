f9fba3159a2d9aba63c35568b35d1302574b4735
Bryan Alejandro Cubillos Prieto <bryant_alejandro@hotmail.com>
2025-07-08 05:19:27 -0500

    UI:MOB: Init new project

---
+++ lib/features/welcome/page/welcome-page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class WelcopmePage extends StatefulWidget {
+  const WelcopmePage({super.key});
+
+  @override
+  State<WelcopmePage> createState() => _WelcopmePageState();
+}
+
+class _WelcopmePageState extends State<WelcopmePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
